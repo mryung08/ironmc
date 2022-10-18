660464f0de9084c222fc6e10dc5301cc32204590
YungTimo12 <demonnetwork120@gmail.com>
2022-10-18 10:13:54 +0200

    test

---
+++ ticket-bot-main/events/interactionCreate.js
@@ -0,0 +1,291 @@
+let hastebin = require('hastebin');
+
+module.exports = {
+  name: 'interactionCreate',
+  async execute(interaction, client) {
+    if (!interaction.isButton()) return;
+    if (interaction.customId == "open-ticket") {
+      if (client.guilds.cache.get(interaction.guildId).channels.cache.find(c => c.topic == interaction.user.id)) {
+        return interaction.reply({
+          content: 'hai già un Ticket creato!',
+          ephemeral: true
+        });
+      };
+
+      interaction.guild.channels.create(`ticket-${interaction.user.username}`, {
+        parent: client.config.parentOpened,
+        topic: interaction.user.id,
+        permissionOverwrites: [{
+            id: interaction.user.id,
+            allow: ['SEND_MESSAGES', 'VIEW_CHANNEL', 'READ_MESSAGE_HISTORY'],
+          },
+          {
+            id: client.config.roleSupport,
+            allow: ['SEND_MESSAGES', 'VIEW_CHANNEL', 'READ_MESSAGE_HISTORY'],
+          },
+          {
+            id: interaction.guild.roles.everyone,
+            deny: ['VIEW_CHANNEL'],
+          },
+        ],
+        type: 'text',
+      }).then(async c => {
+        interaction.reply({
+          content: `Ticket è stato creato! <#${c.id}>`,
+          ephemeral: true
+        });
+
+        const embed = new client.discord.MessageEmbed()
+          .setColor('ffffff')
+          .setAuthor('Reason', ' ')
+          .setDescription('scegli un motivo per cui apri il ticket')
+          .setFooter('Ticket System', ' ')
+          .setTimestamp();
+
+        const row = new client.discord.MessageActionRow()
+          .addComponents(
+            new client.discord.MessageSelectMenu()
+            .setCustomId('categoria')
+            .setPlaceholder('scegli un motivo per cui apri il ticket')
+            .addOptions([{
+                label: 'Candidatura Staff',
+                value: 'Candidatura Staff',
+                emoji: { name: '📑' }
+              },
+              {
+                label: 'Supporto',
+                value: 'Supporto',
+                emoji: { name: '❓' }
+              },
+              {
+                label: 'Report',
+                value: 'Report',
+                emoji: { name: '😡' }
+              },
+              {
+                label: 'Partnership',
+                value: 'Partnership',
+                emoji: { name: '🥇' }
+              },
+            ]),
+          );
+
+        msg = await c.send({
+          content: `<@!${interaction.user.id}>`,
+          embeds: [embed],
+          components: [row]
+        });
+
+        const collector = msg.createMessageComponentCollector({
+          componentType: 'SELECT_MENU',
+          time: 20000
+        });
+
+        collector.on('collect', i => {
+          if (i.user.id === interaction.user.id) {
+            if (msg.deletable) {
+              msg.delete().then(async () => {
+                const embed = new client.discord.MessageEmbed()
+                  .setColor('ffffff')
+                  .setAuthor('Ticket', ' ')
+                  .setDescription(`<@!${interaction.user.id}> ha creato un **Ticket** con la ragione ・ ${i.values[0]}`)
+                  .setFooter('Ticket System', ' ')
+                  .setTimestamp();
+
+                const row = new client.discord.MessageActionRow()
+                  .addComponents(
+                    new client.discord.MessageButton()
+                    .setCustomId('close-ticket')
+                    .setLabel('close ticket')
+                    .setEmoji('899745362137477181')
+                    .setStyle('DANGER'),
+                  );
+
+                const opened = await c.send({
+                  content: `<@&${client.config.roleSupport}>`,
+                  embeds: [embed],
+                  components: [row]
+                });
+
+                opened.pin().then(() => {
+                  opened.channel.bulkDelete(1);
+                });
+              });
+            };
+            if (i.values[0] == 'Candidatura Staff') {
+              c.edit({
+                parent: client.config.parentApply
+              });
+            };
+            if (i.values[0] == 'Supporto') {
+              c.edit({
+                parent: client.config.parentSupport
+              });
+            };
+            if (i.values[0] == 'Report') {
+              c.edit({
+                parent: client.config.parentComplaint
+              });
+            };
+            if (i.values[0] == 'Partnership') {
+              c.edit({
+                parent: client.config.parentPartnership
+              });
+            };
+          };
+        });
+
+        collector.on('end', collected => {
+          if (collected.size < 1) {
+            c.send(`There was no reason, the ticket will be closed.`).then(() => {
+              setTimeout(() => {
+                if (c.deletable) {
+                  c.delete();
+                };
+              }, 100000);
+            });
+          };
+        });
+      });
+    };
+
+    if (interaction.customId == "close-ticket") {
+      const guild = client.guilds.cache.get(interaction.guildId);
+      const chan = guild.channels.cache.get(interaction.channelId);
+
+      const row = new client.discord.MessageActionRow()
+        .addComponents(
+          new client.discord.MessageButton()
+          .setCustomId('confirm-close')
+          .setLabel('Ticket close')
+          .setStyle('DANGER'),
+          new client.discord.MessageButton()
+          .setCustomId('no')
+          .setLabel('close cancel')
+          .setStyle('SECONDARY'),
+        );
+
+      const verif = await interaction.reply({
+        content: 'Are you sure you want to close the ticket?',
+        components: [row]
+      });
+
+      const collector = interaction.channel.createMessageComponentCollector({
+        componentType: 'BUTTON',
+        time: 10000
+      });
+
+      collector.on('collect', i => {
+        if (i.customId == 'confirm-close') {
+          interaction.editReply({
+            content: `The ticket has been closed by <@!${interaction.user.id}>`,
+            components: []
+          });
+
+          chan.edit({
+              name: `closed-${chan.name}`,
+              permissionOverwrites: [
+                {
+                  id: client.users.cache.get(chan.topic),
+                  deny: ['SEND_MESSAGES', 'VIEW_CHANNEL'],
+                },
+                {
+                  id: client.config.roleSupport,
+                  allow: ['SEND_MESSAGES', 'VIEW_CHANNEL'],
+                },
+                {
+                  id: interaction.guild.roles.everyone,
+                  deny: ['VIEW_CHANNEL'],
+                },
+              ],
+            })
+            .then(async () => {
+              const embed = new client.discord.MessageEmbed()
+                .setColor('ffffff')
+                .setAuthor('Ticket', ' ')
+                .setDescription('```Ticket saving```')
+                .setFooter('Ticket System', ' ')
+                .setTimestamp();
+
+              const row = new client.discord.MessageActionRow()
+                .addComponents(
+                  new client.discord.MessageButton()
+                  .setCustomId('delete-ticket')
+                  .setLabel('Ticket delete')
+                  .setEmoji('🗑️')
+                  .setStyle('DANGER'),
+                );
+
+              chan.send({
+                embeds: [embed],
+                components: [row]
+              });
+            });
+
+          collector.stop();
+        };
+        if (i.customId == 'no') {
+          interaction.editReply({
+            content: 'Close ticket cancelled!',
+            components: []
+          });
+          collector.stop();
+        };
+      });
+
+      collector.on('end', (i) => {
+        if (i.size < 1) {
+          interaction.editReply({
+            content: 'Ticket closure cancelled!',
+            components: []
+          });
+        };
+      });
+    };
+
+    if (interaction.customId == "delete-ticket") {
+      const guild = client.guilds.cache.get(interaction.guildId);
+      const chan = guild.channels.cache.get(interaction.channelId);
+
+      interaction.reply({
+        content: 'ticket saving...'
+      });
+
+      chan.messages.fetch().then(async (messages) => {
+        let a = messages.filter(m => m.author.bot !== true).map(m =>
+          `${new Date(m.createdTimestamp).toLocaleString('de-DE')} - ${m.author.username}#${m.author.discriminator}: ${m.attachments.size > 0 ? m.attachments.first().proxyURL : m.content}`
+        ).reverse().join('\n');
+        if (a.length < 1) a = "It was not written in the ticket"
+        hastebin.createPaste(a, {
+            contentType: 'text/plain',
+            server: 'https://hastebin.com'
+          }, {})
+          .then(function (urlToPaste) {
+            const embed = new client.discord.MessageEmbed()
+              .setAuthor('Logs Ticket', ' ')
+              .setDescription(`📰 Ticket-Logs \`${chan.id}\` created by <@!${chan.topic}> and deleted by <@!${interaction.user.id}>\n\nLogs: [**Click here to see the logs**](${urlToPaste})`)
+              .setColor('2f3136')
+              .setTimestamp();
+
+            const embed2 = new client.discord.MessageEmbed()
+              .setAuthor('Logs Ticket', ' ')
+              .setDescription(`📰 Logs of your ticket \`${chan.id}\`: [**Click here to see the logsn**](${urlToPaste})`)
+              .setColor('2f3136')
+              .setTimestamp();
+
+            client.channels.cache.get(client.config.logsTicket).send({
+              embeds: [embed]
+            });
+            client.users.cache.get(chan.topic).send({
+              embeds: [embed2]
+            }).catch(() => {console.log('I cant send it DM')});
+            chan.send('Delete channel.');
+
+            setTimeout(() => {
+              chan.delete();
+            }, 5000);
+          });
+      });
+    };
+  },
+};
---
+++ ticket-bot-main/events/ready.js
@@ -0,0 +1,87 @@
+module.exports = {
+  name: 'ready',
+  async execute(client) {
+    console.log('Bot Online!')
+    console.log('Bot Dev MrYung_');
+    const oniChan = client.channels.cache.get(client.config.ticketChannel)
+
+    function sendTicketMSG() {
+      const embed = new client.discord.MessageEmbed()
+        .setColor('ffffff')
+        .setAuthor('SUPPORT TICKET', client.user.avatarURL())
+        .setDescription('**TICKET**\nApri un ticket in base alle tue esigenze,\ncliccare con il tasto destro il button\n\n• 🚔 Candidatura Staff \n• 🚧​ Report\n• 👮🏻‍♂️​ Supporto\n• 🟣​ Partnership\n\nNon abusate del Ticket se / ban.')
+        .setFooter(client.config.footerText, client.user.avatarURL())
+      const row = new client.discord.MessageActionRow()
+        .addComponents(
+          new client.discord.MessageButton()
+          .setCustomId('open-ticket')
+          .setLabel('')
+          .setEmoji('🎫')
+          .setStyle('PRIMARY'),
+        );
+
+      oniChan.send({
+        embeds: [embed],
+        components: [row]
+      })
+    }
+
+    const toDelete = 10000;
+
+    async function fetchMore(channel, limit) {
+      if (!channel) {
+        throw new Error(`Kanal created ${typeof channel}.`);
+      }
+      if (limit <= 100) {
+        return channel.messages.fetch({
+          limit
+        });
+      }
+
+      let collection = [];
+      let lastId = null;
+      let options = {};
+      let remaining = limit;
+
+      while (remaining > 0) {
+        options.limit = remaining > 100 ? 100 : remaining;
+        remaining = remaining > 100 ? remaining - 100 : 0;
+
+        if (lastId) {
+          options.before = lastId;
+        }
+
+        let messages = await channel.messages.fetch(options);
+
+        if (!messages.last()) {
+          break;
+        }
+
+        collection = collection.concat(messages);
+        lastId = messages.last().id;
+      }
+      collection.remaining = remaining;
+
+      return collection;
+    }
+
+    const list = await fetchMore(oniChan, toDelete);
+
+    let i = 1;
+
+    list.forEach(underList => {
+      underList.forEach(msg => {
+        i++;
+        if (i < toDelete) {
+          setTimeout(function () {
+            msg.delete()
+          }, 1000 * i)
+        }
+      })
+    })
+
+    setTimeout(() => {
+      sendTicketMSG()
+    }, i);
+  },
+};
